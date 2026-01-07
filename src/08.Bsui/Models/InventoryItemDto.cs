90ef4e2886d7a4f9829e0a2603515aacbfbedd68
MuhammadSuheil <suheilputra03@gmail.com>
2026-01-07 11:09:24 +0700

    refactor: inventory.razor MVVM implementation

---
+++ src/08.Bsui/Models/InventoryItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pertamina.SolutionTemplate.Bsui.Models
+{
+    public class InventoryItemDto
+    {
+        public string Nama { get; set; } = "";
+        public string NoRak { get; set; } = "";
+        public int Stok { get; set; }
+        public string Satuan { get; set; } = "Pcs";
+        public string ImageUrl { get; set; } = "";
+        public DateTime? ExpDate { get; set; }
+        public bool IsExpiredSoon => ExpDate.HasValue && (ExpDate.Value - DateTime.Now).TotalDays < 30;
+    }
+}
